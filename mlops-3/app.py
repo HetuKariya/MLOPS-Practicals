f68bad6463f3c99ecf6dc7cda13cce66db452460
HetuKariya <131229641+HetuKariya@users.noreply.github.com>
2026-08-18 18:00:26 +0530

    Fixed MLOPS-3

--- mlops-3/app.py
+++ mlops-3/app.py
@@ -45,4 +45,4 @@ def predict():
     return render_template("result.html", prediction=prediction, inputs=inputs)
 
 if __name__ == "__main__":
-    serve(app,host="0.0.0.0",port=8080)
+    serve(app,host="0.0.0.0",port=5001)
